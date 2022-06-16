8268cafb8725d04c55be6bd532040b45afab266c
Komarudin <101499377+komarcrew@users.noreply.github.com>
2022-06-16 10:29:20 -0700

    Add files via upload

---
+++ admin_footer.php
@@ -0,0 +1,6 @@
+<footer>
+	<p>&copy; 2022 - Universitas Pelita Bangsa - Komarudin - 312010068</p>
+</footer>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,55 +1,18 @@
-<?php
+<?= $this->include('template/header'); ?>
 
-// Path to the front controller (this file)
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+<?php if($artikel): foreach($artikel as $row): ?>
+    <article class="entry">
+        <h2
+        <a href="<?= base_url('/artikel/' . $row['slug']);?>"><?=$row['judul']; ?></a>
+        </h2>
+        <img src="<?= base_url('/gambar/' . $row['gambar']);?>" alt="<?=$row['judul']; ?>">
+        <p><?= substr($row['isi'], 0, 200); ?></p>
+    </article>
+<hr class="divider" />
+<?php endforeach; else: ?>
+<article class="entry">
+    <h2>Belum ada data.</h2>
+</article>
+<?php endif; ?>
 
-// Ensure the current directory is pointing to the front controller's directory
-chdir(FCPATH);
-
-/*
- *---------------------------------------------------------------
- * BOOTSTRAP THE APPLICATION
- *---------------------------------------------------------------
- * This process sets up the path constants, loads and registers
- * our autoloader, along with Composer's, loads our constants
- * and fires up an environment-specific bootstrapping.
- */
-
-// Load our paths config file
-// This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../app/Config/Paths.php';
-// ^^^ Change this line if you move your application folder
-
-$paths = new Config\Paths();
-
-// Location of the framework bootstrap file.
-require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
-
-// Load environment settings from .env files into $_SERVER and $_ENV
-require_once SYSTEMPATH . 'Config/DotEnv.php';
-(new CodeIgniter\Config\DotEnv(ROOTPATH))->load();
-
-/*
- * ---------------------------------------------------------------
- * GRAB OUR CODEIGNITER INSTANCE
- * ---------------------------------------------------------------
- *
- * The CodeIgniter class contains the core functionality to make
- * the application run, and does all of the dirty work to get
- * the pieces all working together.
- */
-
-$app = Config\Services::codeigniter();
-$app->initialize();
-$context = is_cli() ? 'php-cli' : 'web';
-$app->setContext($context);
-
-/*
- *---------------------------------------------------------------
- * LAUNCH THE APPLICATION
- *---------------------------------------------------------------
- * Now that everything is setup, it's time to actually fire
- * up the engines and make this app do its thang.
- */
-
-$app->run();
+<?= $this->include('template/footer'); ?>
